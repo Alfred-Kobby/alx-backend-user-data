5bef4679b4b00efb4db0200e7a9c5a2e3a20425e
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-09-05 15:45:45 +0000

    auth

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+class Auth:
+    """ Authentication """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Checks if API routes require authentication """
+        if path is None or not excluded_paths:
+            return True
+        for i in excluded_paths:
+            if i.endswith('*') and path.startswith(i[:-1]):
+                return False
+            elif i in {path, path + '/'}:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Checks if Authorization request header is present
+        & contains values """
+        if request is None or "Authorization" not in request.headers:
+            return None
+        else:
+            return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ placeholder """
+        return None
+    
